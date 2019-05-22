163cb0faefb3d7aee272b11006e6e43e917e07a1
LabVision01 <LabVision01@192.168.0.106>
2019-05-22 17:42:27 -0500

    actualizacion1

--- StimSD/src/main/java/com/lab/sdt/service/ConsultaUsuarios.java
+++ StimSD/src/main/java/com/lab/sdt/service/ConsultaUsuarios.java
@@ -1,6 +1,7 @@
 package com.lab.sdt.service;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,10 +18,24 @@ public class ConsultaUsuarios {
 	@Autowired
 	private UsuarioMapper usuarioMapper;
 	
-	public void insertarHistoricoDeGeneracionCodigos(String username) throws Exception{
-		Usuario user_1 = new Usuario();
+	public void insertarUsuario(String username) throws Exception{
+	/*	Usuario user_1 = new Usuario();
 		user_1.setNombre("pedro_el");
-		insertarUsuario(user_1);
+		user_1.setApellido1("mama");
+		user_1.setApellido2("jdoej");
+		user_1.setCalle("aduios");
+		user_1.setNum("11");
+		user_1.setColonia("esoerabza");
+		user_1.setCodigopostal("57800");
+		user_1.setIdestado(2);
+		user_1.setTelefono("pedro_el");
+		user_1.setEmail("pedro_el");
+		user_1.setIdtipo(3);
+		user_1.setCuenta("pedro_el");
+		user_1.setContrasenia("pedro_el");
+		user_1.setEstatus("pedro_el");
+		
+		insertarUsuario(user_1);*/
 	}
 	public void insertarUsuario(Usuario usuario)throws Exception{
 		usuarioMapper.insert(usuario);
---
+++ StimSD/src/main/java/com/lab/sdt/view/hello.java
@@ -0,0 +1,67 @@
+package com.lab.sdt.view;
+
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import com.lab.sdt.util.MensajeG;
+
+import com.lab.sdt.service.ConsultaUsuarios;
+@ManagedBean(name = "editor")
+@SessionScoped
+public class hello implements Serializable {
+ 
+private static final long serialVersionUID = 1L;
+ 
+@ManagedProperty("#{consultaUsuarios}")
+private ConsultaUsuarios consultaUsuarios;
+
+private String name = "1";
+
+private String apellido = "2";
+
+private String resultado ="";
+
+@PostConstruct
+public void init(){
+	
+}
+
+public void muestra() {
+	try {
+		consultaUsuarios.insertarUsuario("pedro");
+	MensajeG.mostrar("se muestra", FacesMessage.SEVERITY_WARN);
+	}catch(Exception e) {
+		MensajeG.mostrar(e.toString(), FacesMessage.SEVERITY_WARN);
+	}
+	
+	setResultado("Prueba base de datos");
+}
+
+
+public String getResultado() {
+	return resultado;
+}
+
+public void setResultado(String resultado) {
+	this.resultado = resultado;
+}
+
+
+
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+}
